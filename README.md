9fc7c9f5da5bda5efe97a7c088bfa4e7f67d7bdb
Cyandied <awesome.pie753@gmail.com>
2023-07-29 17:42:37 +0200

    Images now resizes on small zoom levels!

--- README.md
+++ README.md
@@ -10,11 +10,15 @@ Usually, this include some downscaling of the quality when you zoom out, and inc
 Zoom level is how far you wish to be able to zoom into your image. think of it as how many time we cut your image up in perfectly square areas. Zoom level 0 is your whole image, with 0 cuts, while zoom level 1 is your whole image cut once horizontally and once vertically.
 
 To figure out how many tiles you will get for a given zoom level, use the following formula:
-$$ n = 2^z $$
+
+$ n = 2^z $
+
 Where $n$ is the number of tiles and $z$ is the desired zoom level.
 
 To get the correct size for your maximum zoom level, ensure that your full image has the size PxP, where P is defined as:
-$$ P = 256 * 2^z $$
+
+$ P = 256 * 2^z $
+
 and z is defined as your maximum zoom level.
 
 ## How to use this program
@@ -29,10 +33,10 @@ and z is defined as your maximum zoom level.
 - Define your zoom level by changing the number in the function ```runProgram(x)```, the default is 4
 - Run the program with ```python program.py```
 - Find your tiles in the ***result*** folder!
-
+    - Remember to void this folder if you want to run the program again!
 ## Why can I not use any size image I want?
 I am making this to work easily with [Leaflet](https://leafletjs.com/), and I enjoy using common conventions.
 
 So:
-- Perfectly square map ensures that coordinated work without issues in [Leaflet](https://leafletjs.com/)
+- Perfectly square map ensures that coordinates work without issues in [Leaflet](https://leafletjs.com/)
 - Convention for other tile-cutting programs is that the smallest tile is 256x256 pixels
--- functions.py
+++ functions.py
@@ -18,34 +18,28 @@ def checkImage(img:Img) -> list[bool,str]:
         return False, "\nImage size MUST be some multiple of 256!"
     return True, "\nImage correct proportion and size, cutting begins now..."
 
-def cut(zoom_lvl:int):
+def cut(zoom_lvl:int, img:Img):
     tiles = []
-    for x_coord in listdir(join("result",str(zoom_lvl-1))):
-        right = []
-        left = []
-        for y_coord in listdir(join("result",str(zoom_lvl-1),x_coord)):
-            img = Image.open(join("result",str(zoom_lvl-1),x_coord,y_coord))
-            this_right, this_left = crop(img)
-            for i in range(0,2):
-                right.append(this_right[i])
-                left.append(this_left[i])
-        tiles.append(right)
-        tiles.append(left)
+    nr_of_tiles = 2**zoom_lvl
+    w, h = img.size
+    d = w/nr_of_tiles
+
+    for i in range(0,nr_of_tiles):
+        x_layer = []
+        for j in range(0,nr_of_tiles):
+            box = (i*d, j*d, i*d+d, j*d+d)
+            x_layer.append(img.crop(box))
+        tiles.append(x_layer)
+
+
     for i,x_set in enumerate(tiles):
         mkdir(join("result",str(zoom_lvl),str(i)))
         for j,y_img in enumerate(x_set):
+            w,h = y_img.size
+            if w/2 > 256:
+                y_img = y_img.resize((int(w/2),int(h/2)))
             y_img.save(join("result",str(zoom_lvl),str(i),str(j)+".png"),"png")
     return
-
-def crop(img:Img) -> list[list[Img,Img],list[Img,Img]]:
-    h,w = img.size
-
-    ur = img.crop((0,0,w/2,h/2))
-    ul = img.crop((w/2,0,w,h/2))
-
-    lr = img.crop((0,h/2,w/2,h))
-    ll = img.crop((w/2,h/2,w,h))
-    return [ur,lr],[ul,ll]
 
 def runProgram(max_zoom:int):
     source_img_name = listdir("image_to_cut")
@@ -57,11 +51,13 @@ def runProgram(max_zoom:int):
         zoom_level = 0
         mkdir(join("result",str(zoom_level)))
         mkdir(join("result",str(zoom_level),"0"))
-        source_img.save(join("result","0","0","0.png"),"png")
+        w,h = source_img.size
+        to_save = source_img.resize((int(w/2),int(h/2)))
+        to_save.save(join("result","0","0","0.png"),"png")
         zoom_level += 1
         while zoom_level <= max_zoom:
             mkdir(join("result",str(zoom_level)))
             print(f'\nCutting zoom level {zoom_level}...')
-            cut(zoom_level)
+            cut(zoom_level,source_img)
             zoom_level += 1
         print("\nFinished! Find your tiles in the results folder!")
